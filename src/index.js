4bc67543b0df1a770e14d18f6755d87721e827a4
Erika Ferreira <eriferreira777@gmail.com>
2026-05-14 08:38:01 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,18 @@ const myWhishList = [];
 
 console.log("Welcome to the your Shopee Cart!");
 
-//criando dois itens
-const item1 = await createItem("hotwheels ferrari", 20.99, 1);
-const item2 = await createItem("hotwheels lamborghini", 39.99, 3);
+const produtos = [
+  { id: 1, nome: "Boneca Moranguinho", preco: 89.9 },
+  { id: 2, nome: "Pelucia Moranguinho", preco: 69.9 },
+  { id: 3, nome: "Mochila Moranguinho", preco: 119.9 },
+  { id: 4, nome: "Estojo Moranguinho", preco: 34.9 },
+  { id: 5, nome: "Caderno Moranguinho", preco: 29.9 },
+  { id: 6, nome: "Caneca Moranguinho", preco: 39.9 },
+  { id: 7, nome: "Camiseta Moranguinho", preco: 59.9 },
+  { id: 8, nome: "Chaveiro Moranguinho", preco: 19.9 },
+  { id: 9, nome: "Adesivos Moranguinho", preco: 14.9 },
+  { id: 10, nome: "Kit Festa Moranguinho", preco: 99.9 },
+];
 
 // adicionei dois itens ao carrinho
 await cartService.addItem(myCart, item1);
